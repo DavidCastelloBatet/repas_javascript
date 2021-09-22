4fc5a831ec69dffed589d343d44d19c9e3cec4af
David Castello <davidcastellobatetgmail.com>
2021-09-22 10:47:32 +0200

    template strings

--- src/bases/01-const-let.js
+++ src/bases/01-const-let.js
@@ -1,5 +1,8 @@
 // Variables y constantes
-
+// NO usar var
+// Usar const cuando sepamos que no va a cambiar el valor
+// de la variable.
+// Usar let cuando el valor vaya a cambiar.
 const nombre = 'Fernando';
 const apellido = 'Herrera';
 
@@ -8,7 +11,11 @@ valorDado = 4;
 
 
 console.log(nombre, apellido, valorDado);
-
+// variables de scope (ámbito o contexto de uso de
+// la variable)
+// en este caso vemos que const nombre ya existe
+// globalmente pero la podemos usar en este scope
+//
 if (true) {
   const nombre = 'Peter';
   console.log(nombre);
--- src/bases/02-template-string.js
+++ src/bases/02-template-string.js
@@ -1,12 +1,25 @@
+// Template strings podemos concatenar strings, podemos
+// poner variables, poner operaciones o resultados de
+// operaciones.
+// Se usa el acento abierto (backtick), donde ponemos el
+// string y usamos ${variable o operacion} para 
+// insertar variables u operaciones.
 const nom = 'David';
 const edad = 5;
 
-const pres = (`Hola ${nom} i tinc ${edad + 45} anys`);
-
+const pres = `Hola ${nom} i tinc ${edad + 45} anys`;
 console.log(pres);
 
-function getSaludo(nom) {
-  return `Hola ${nom}, com estas ?`
+// posibilidad de hacerlo multilinea
+const pres1= `
+Hola em dic ${nom} 
+i tinc 
+${edad + 45} anys`;
+console.log(pres1);
+
+// como retorno de una funcion
+function getSaludo(nomRebutParametre) {
+  return `Hola ${nomRebutParametre}, com estas ?`
 }
 
 console.log(getSaludo(nom));
