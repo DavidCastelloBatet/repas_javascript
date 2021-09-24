27b00fac14e7921113bcbac69a4f71d7f54bcd8e
David Castello <davidcastellobatetgmail.com>
2021-09-24 21:28:32 +0200

    import-export-find-filter

---
+++ src/data/heroes.js
@@ -0,0 +1,41 @@
+
+// Funciona con bases / 08-imp....
+// exportacion por defecto
+const heroes = [ 
+  {
+      id: 1,
+      name: 'Batman',
+      owner: 'DC'
+  },
+  {
+      id: 2,
+      name: 'Spiderman',
+      owner: 'Marvel'
+  },
+  {
+      id: 3,
+      name: 'Superman',
+      owner: 'DC'
+  },
+  {
+      id: 4,
+      name: 'Flash',
+      owner: 'DC'
+  },
+  {
+      id: 5,
+      name: 'Wolverine',
+      owner: 'Marvel'
+  },
+];
+// exportacion por defecto
+export default heroes;
+// exportacion individual
+export const jefes = ['Marvel', 'DC'];
+// exportacion conjunta en un objeto
+const mesos = ['gener', 'febrer', 'març'];
+const dies = ['dilluns', 'dimarts', 'dimecres'];
+export {
+  mesos,
+  dies,
+}
