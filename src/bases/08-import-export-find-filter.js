2b5a6ae01fa434290b6e93cc03feaff788928d22
David <davidcastellobatet@gmail.com>
2021-09-25 03:39:59 +0200

    promesas

--- src/bases/08-import-export-find-filter.js
+++ src/bases/08-import-export-find-filter.js
@@ -1,17 +1,18 @@
 // Funciona con data/heroes.js
 // Importacion por defecto (le podemos dar el nombre que queramos)
-import heroes from './data/heroes';
+import heroes from '../data/heroes';
 // importacion individual
-import {jefes} from './data/heroes';
+import {jefes} from '../data/heroes';
 // importacion conjunta desestructurando un objeto
-import {mesos, dies} from './data/heroes';
+import {mesos, dies} from '../data/heroes';
 console.log(heroes);
 console.log(jefes);
 console.log(mesos, dies);
 
 // uso del metodo find(). devuelve el primer elemento
 // del array que coincide con la condicion.
-const getHeroeById = (id) => {
+// export para usarlo en bases / 09-....
+export const getHeroeById = (id) => {
   return heroes.find(heroe => heroe.id === id);
 }
 const heroe = getHeroeById(1);
@@ -19,7 +20,8 @@ console.log(heroe);
 
 // uso de filter(). devuelve un array con todos los 
 //elementos que coinciden con la condicion.
+// export para usarlo en bases / 09-....
 
-const getHeroeByOwner = (owner) => heroes.filter(heroe => heroe.owner === owner);
+export const getHeroeByOwner = (owner) => heroes.filter(heroe => heroe.owner === owner);
 const owners = getHeroeByOwner('DC');
 console.log(owners);
--- src/data/heroes.js
+++ src/data/heroes.js
@@ -2,31 +2,31 @@
 // Funciona con bases / 08-imp....
 // exportacion por defecto
 const heroes = [ 
-  {
-      id: 1,
-      name: 'Batman',
-      owner: 'DC'
-  },
-  {
-      id: 2,
-      name: 'Spiderman',
-      owner: 'Marvel'
-  },
-  {
-      id: 3,
-      name: 'Superman',
-      owner: 'DC'
-  },
-  {
-      id: 4,
-      name: 'Flash',
-      owner: 'DC'
-  },
-  {
-      id: 5,
-      name: 'Wolverine',
-      owner: 'Marvel'
-  },
+    {
+        id: 1,
+        name: 'Batman',
+        owner: 'DC'
+    },
+    {
+        id: 2,
+        name: 'Spiderman',
+        owner: 'Marvel'
+    },
+    {
+        id: 3,
+        name: 'Superman',
+        owner: 'DC'
+    },
+    {
+        id: 4,
+        name: 'Flash',
+        owner: 'DC'
+    },
+    {
+        id: 5,
+        name: 'Wolverine',
+        owner: 'Marvel'
+    },
 ];
 // exportacion por defecto
 export default heroes;
@@ -36,6 +36,6 @@ export const jefes = ['Marvel', 'DC'];
 const mesos = ['gener', 'febrer', 'març'];
 const dies = ['dilluns', 'dimarts', 'dimecres'];
 export {
-  mesos,
-  dies,
+    mesos,
+    dies,
 }
